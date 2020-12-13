d280e238db9a8540cc966df7effd94227babe4c3
BobrikovAleksey <bobrikov.spb@ya.ru>
2020-12-14 02:33:07 +0300

    fixed bugs with js modules ( #9 )

--- scripts/app.js
+++ scripts/app.js
@@ -1,24 +1,24 @@
 'use strict';
 
-import bulletList from '../components/bullet-list.js';
-import checkboxList from '../components/checkbox-list.js';
-import cmSelect from '../components/cm-select.js';
-import cmSelectExpanded from '../components/cm-select-expanded.js';
+// import bulletList from '../components/bullet-list.js';
+// import checkboxList from '../components/checkbox-list.js';
+// import cmSelect from '../components/cm-select.js';
+// import cmSelectExpanded from '../components/cm-select-expanded.js';
 import cmHeader from '../components/cm-header.js';
-import likeButton from '../components/like-button.js';
-import radioGroup from '../components/radio-group.js';
-import toggle from '../components/toggle.js';
+// import likeButton from '../components/like-button.js';
+// import radioGroup from '../components/radio-group.js';
+// import toggle from '../components/toggle.js';
 
 const app = {
     components: {
-        'bullet-list': bulletList,
-        'checkbox-list': checkboxList,
-        'cm-select': cmSelect,
-        cmSelectExpanded,
+        // 'bullet-list': bulletList,
+        // 'checkbox-list': checkboxList,
+        // 'cm-select': cmSelect,
+        // cmSelectExpanded,
         cmHeader,
-        'like-button': likeButton,
-        'radio-group': radioGroup,
-        toggle,
+        // 'like-button': likeButton,
+        // 'radio-group': radioGroup,
+        // toggle,
     },
     refs: {},
 
